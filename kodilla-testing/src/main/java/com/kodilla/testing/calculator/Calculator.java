f65fef610693a9ae9d377e3c1f061a9f24958bfc
yelek <Dawid.badurakrk@gmail.com>
2020-03-19 14:47:23 +0100

    third commit

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,16 +1,11 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-    int a, b;
 
     public int add(int a, int b) {
-        this.a=a;
-        this.b=b;
         return a+b;
     }
     public int subtract(int a, int b) {
-        this.a=a;
-        this.b=b;
         return a-b;
     }
 }
