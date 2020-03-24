744845f3b138c71d07e8af3c1272ccea34bd93e5
yelek <Dawid.badurakrk@gmail.com>
2020-03-24 19:07:10 +0100

    immutable

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeafutifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 
 public class StreamMain {
@@ -17,6 +18,11 @@ public class StreamMain {
         System.out.println(poemBeafutifier.beautify("Taki sobie text",text -> "ABC"+text+"CBA"));
         System.out.println(poemBeafutifier.beautify("Taki sobie text",text -> text.replace('e','B')));
         System.out.println(poemBeafutifier.beautify("Taki sobie text",text -> text.replaceFirst("s","BrrrB")));
+
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
 
 
     }
