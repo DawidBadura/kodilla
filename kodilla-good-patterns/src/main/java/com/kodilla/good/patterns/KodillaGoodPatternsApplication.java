26c85a3faf8f7d6071c9cb6d65adc41cd6620737
yelek <Dawid.badurakrk@gmail.com>
2020-05-21 12:35:07 +0200

    First Commit

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/KodillaGoodPatternsApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla.good.patterns;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaGoodPatternsApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println(MovieStore.printBooks());
+
+    }
+
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/MovieStore.java
@@ -0,0 +1,34 @@
+package com.kodilla.good.patterns;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class MovieStore {
+
+    static public Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+    static public String printBooks(){
+        return getMovies().entrySet().stream().flatMap(e->e.getValue().stream()).collect(Collectors.joining(" ! "));
+
+    }
+}
+
