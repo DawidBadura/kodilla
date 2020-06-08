00f698e9f062908d0fabe73f50167e9919ab6f40
yelek <Dawid.badurakrk@gmail.com>
2020-06-08 17:23:53 +0200

    last

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+
+    @Test
+    public void testBigmac() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("Onion")
+                .burgers(2)
+                .bun("Bold")
+                .sauce("Standard")
+                .ingredient("Ham")
+                .ingredient("Mushrooms")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(3, howManyIngredients);
+    }
+}
